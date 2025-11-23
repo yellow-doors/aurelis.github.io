60fcccf329924efc0cb47ec5cf9709ef53d0512b
yellow-doors <yellowdoors.art@gmail.com>
2025-11-22 20:33:22 -0800

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "Aurelis",
+  url: "https://yellow-doors.github.io/aurelis/",
+  description: `???.`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1MYyVZLHppfi9n6pnR8hMUzE1imlzQ52PRLFJAZ9hB_M",
 
   pages: {
     masterlist:    "masterlist",
@@ -42,11 +42,9 @@ charadex.sheet = {
 
   options: {
 
-    designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
-    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
-    itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
+    designTypes: ['All', 'Fools Gold', 'Herrings Call', 'NPC', 'Deity'],
+    magic: ['All', 'Life', 'Elements', 'Psyche'],
+    itemTypes: ['All', 'Currency', 'Equip - Head', 'Equip - Body', 'Equip - Legs' 'Equip - Feet'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
   }
